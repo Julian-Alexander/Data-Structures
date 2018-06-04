a776f99647d0ea54df1cb8cf8f80024b610fb0c4
JulianAlexander <julian.pineyro@gmail.com>
2018-06-05 01:59:11 +0300

    Linked List done

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,26 +9,52 @@ class LinkedList {
   of the list. The `tail` pointer
   should be updated accordingly */
   addToTail(value) {
-
+    const newNode = {
+      value,
+      next: null
+    };
+    if(this.tail) {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
   }
 
   /* Remove the list's `head` value 
   The `head` pointer should be updated
   accordingly */
   removeHead() {
-
+    const head = this.head.value;
+    this.head = this.head.next;
+    return head;
   }
 
   /* Searches the list for the given value
   Returns true or false accordingly */
   contains(value) {
-
+    let node = this.head;
+    while (node){
+      if (node.value === value) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+    return false;
   }
 
   /* Finds and returns the maximal value
   of all the values in the list */
   getMax() {
-    
+    if (!this.head) return null;
+    let node = this.head;
+    let max = this.head.value;
+    while(node) {
+      if (node.value > max) {
+        max = node.value;
+      }
+      node = node.next;
+    }
+    return max;
   }
 }
 
